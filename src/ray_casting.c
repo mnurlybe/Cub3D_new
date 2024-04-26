f3c1eb7d2c5d58bef5d3779c13fb203dae1add51
lwoiton <l.woiton@gmx.de>
2024-04-26 14:19:29 +0200

    Initiate raycasting

---
+++ src/ray_casting.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray_casting.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwoiton <lwoiton@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 11:52:40 by lwoiton           #+#    #+#             */
+/*   Updated: 2024/04/26 14:13:05 by lwoiton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+
+//how to cast a ray properly into a specific direction?
+// What kind of consts do I need? What structures do I need?
+
+//Applying the DDA (Digital Differential Analysis) Algorithm
+//to determing if the ray hit a border
+
+
